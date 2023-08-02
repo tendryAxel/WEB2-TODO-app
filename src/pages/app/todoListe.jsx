1319a1820bd7d43ba482942ca7eddfbdc0ec83b0
tendryAxel <hei.tendry@gmail.com>
2023-08-02 16:12:36 +0300

    Feat : update functionality

--- src/pages/app/todoListe.jsx
+++ src/pages/app/todoListe.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 export default function ListeTodo({ liste, removeInListe, updateInListe }) {
+    const [inputValue, setInput, clearInput] = useInput()
+
     return (
         <ul>
+            <input type="text" value={inputValue} onChange={setInput} />
             {liste.map((item, index) => {
                 return <li key={index}>
                     <p>{index} - {item}</p>
                     <button onClick={()=>removeInListe(index)}>X</button>
-                    <button onClick={()=>updateInListe(index, input)}>U</button>
+                    <button onClick={()=>{
+                        updateInListe(index, inputValue);
+                        clearInput();
+                        }}>U</button>
                 </li>;
             })}
         </ul>
     );
 }
+
+function useInput () {
+    const [inputValue, setinputValue] = useState("")
+
+    const setInput = (e) => {
+        setinputValue(e.target.value)
+    }
+
+    const clearInput = () => {
+        setinputValue("")
+    }
+
+    return [inputValue, setInput, clearInput]
+}
+  
