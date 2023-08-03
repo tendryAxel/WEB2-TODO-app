3a178901ac99b5f3082f6cceccdab0bdf2a65076
tendryAxel <hei.tendry@gmail.com>
2023-08-03 11:56:25 +0300

    Fix : better instruction

--- src/pages/app/todoListe.jsx
+++ src/pages/app/todoListe.jsx
@@ -16,12 +16,15 @@ export default function ListeTodo({ liste, removeInListe, updateInListe }) {
             <ul className={style.todoListe}>
                 {liste.map((item, index) => {
                     return <li key={index} className={style.listeStyle}>
-                        <p>{index} - {item}</p>
-                        <button onClick={()=>removeInListe(index)}>Delete</button>
-                        <button onClick={()=>{
-                            updateInListe(index, inputValue);
-                            clearInput();
-                            }}>Update this</button>
+                        <p>Todo n°{index} <br />A faire : {item}</p>
+                        <div className={style.buttonSection}>
+                            <button onClick={()=>removeInListe(index)}>Delete</button>
+                            <button onClick={()=>{
+                                updateInListe(index, inputValue);
+                                clearInput();
+                                }}>Update this</button>
+                        </div>
+                        <p>---------------------------------------------------------------------------</p>
                     </li>;
                 })}
             </ul>
